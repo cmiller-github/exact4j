d64967b5d7602d99bea65e9c144a4fa6fb8771d7
Cole Miller <cmiller@pagedna.com>
2019-06-25 10:37:02 -0700

    Support 64-bit transaction tags for firstdata E4

--- src/com/exact/ews/transaction/RestCoder.java
+++ src/com/exact/ews/transaction/RestCoder.java
@@ -85,7 +85,7 @@ class RestCoder implements Coder {
     }
 
     appendElement(document, transactionRoot, "Authorization_Num", r.getAuthorizationNum());
-    appendElement(document, transactionRoot, "Transaction_Tag", ((Integer)r.getTransactionTag()).toString());
+    appendElement(document, transactionRoot, "Transaction_Tag", ((Long)r.getTransactionTag()).toString());
     appendElement(document, transactionRoot, "Reference_No", r.getReferenceNo());
     appendElement(document, transactionRoot, "Customer_Ref", r.getCustomerRef());
     appendElement(document, transactionRoot, "Reference_3", r.getReference3());
